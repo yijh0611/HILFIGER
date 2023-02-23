86a70fc36faa38e5e0e05694c755f57240d6f2b9
yijh0611 <yijh0611@naver.com>
2023-02-23 16:35:35 +0900

    print np shape

--- src/carrot_team/src/sub_img.py
+++ src/carrot_team/src/sub_img.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
 import rospy
+from cv_bridge import CvBridge
 import numpy as np
 
 from sensor_msgs.msg import Image
 
+bridge = CvBridge()
+
+
 def image_callback(msg):
-    print(msg)
+    cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+    # print(msg)
+    print(np.shape(msg))
     
     pass
 
