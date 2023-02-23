4ac4ec43b9e2f7d9cbee30ea9530349935f39133
yijh0611 <yijh0611@naver.com>
2023-02-23 14:25:25 +0900

    edit sh

--- hak.sh
+++ hak.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# ./hak.sh 하지 말고 source ./hak.sh를 해야 한다.
 export DISPLAY='165.194.85.129:0.0'
+echo 'Display changed'
 
 catkin_make
+chmod +x devel/setup.sh
 source devel/setup.sh
+
+chmod +x src/carrot_team/src/control.py
+
+cd ..
+cd startup/challenge/
+
+echo 'End'
