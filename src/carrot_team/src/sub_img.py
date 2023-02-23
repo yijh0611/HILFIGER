75bb3f2680eba34fef2940073a392684bc41524e
yijh0611 <yijh0611@naver.com>
2023-02-23 16:48:02 +0900

    depth map imshow

--- src/carrot_team/src/sub_img.py
+++ src/carrot_team/src/sub_img.py
@@ -3,6 +3,7 @@
 import rospy
 from cv_bridge import CvBridge
 import numpy as np
+import cv2
 
 from sensor_msgs.msg import Image
 
@@ -11,16 +12,21 @@ bridge = CvBridge()
 
 def image_callback(msg):
     img = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-    
+
     cv2.imshow('Drone image', img)
 
-    cv2.waitKey()
-    # cv2.destroyAllWindows()
+    cv2.waitKey(25)
 
-    # pass
+def image_callback_depth(msg):
+    img = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+
+    cv2.imshow('Depth image', img)
+
+    cv2.waitKey(25)
 
 rospy.init_node('image_subscriber', anonymous=True)
 rospy.Subscriber('/red/camera/color/image_raw', Image, image_callback)
+rospy.Subscriber('/red/camera/depth/image_raw', Image, image_callback)
 rospy.spin()
 
 cv2.destroyAllWindows()
