fccc2b56d2885cef8491b8b2167b4e9ef6e5b5f6
yijh0611 <yijh0611@naver.com>
2023-02-23 21:55:49 +0900

    녹화 기능 추가

--- src/carrot_team/src/sub_img.py
+++ src/carrot_team/src/sub_img.py
@@ -12,6 +12,10 @@ bridge_2 = CvBridge()
 
 img = np.array([])
 
+vid_name = 'drone_vid.avi'
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+video = cv2.VideoWriter(vid_name, fourcc, 30.0, (640, 480))
+
 def image_callback(msg):
     global img
     img = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
@@ -23,9 +27,15 @@ def image_callback(msg):
 def image_callback_depth(msg):
     global img
     
+    # get depth image
     img_depth = bridge_2.imgmsg_to_cv2(msg, desired_encoding='passthrough')
     img_depth = img_depth / 10
 
+    # Record
+    global video
+    video.write(img)
+
+    # imshow
     cv2.imshow('Depth image', img_depth)
     cv2.imshow('Drone image', img)
 
