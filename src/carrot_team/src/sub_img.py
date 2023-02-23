148be72f98f699c057c453c64c92aba28f3a1234
yijh0611 <yijh0611@naver.com>
2023-02-23 17:25:18 +0900

    edit error

--- src/carrot_team/src/sub_img.py
+++ src/carrot_team/src/sub_img.py
@@ -10,17 +10,24 @@ from sensor_msgs.msg import Image
 bridge = CvBridge()
 bridge_2 = CvBridge()
 
+img = np.array([])
+
 def image_callback(msg):
+    global img
     img = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
 
-    cv2.imshow('Drone image', img)
+    # cv2.imshow('Drone image', img)
 
-    cv2.waitKey(25)
+    # cv2.waitKey(25)
 
 def image_callback_depth(msg):
+    global img
+    
     img_depth = bridge_2.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+    img_depth = img_depth / 10
 
     cv2.imshow('Depth image', img_depth)
+    cv2.imshow('Drone image', img)
 
     cv2.waitKey(25)
 
