7f1b8290177a543324a12084a5278ac713fa7297
hyun1270 <imbwh@cau.ac.kr>
2023-03-07 19:09:20 +0900

    edit class.cpp

--- src/carrot_team/src/modules/class.cpp
+++ src/carrot_team/src/modules/class.cpp
@@ -8,124 +8,103 @@
 #include "carrot_team/orientation.hpp"
 #include "carrot_team/class.hpp"
 
-class Target_POI
-{
-    private:
-    ros::Subscriber poi_sub_;
-    carrot_team::poi point_of_interests_;
 
-    vector<bool> passed_poi_;
-    int target_idx_;
-    float target_yaw_;
+Target_POI::Target_POI(ros::NodeHandle *nh) {
+    target_idx_ = -1;
+    poi_sub_ = nh->subscribe("/red/poi", 100, &Target_POI::poi_sub_callback, this);
+}
 
-    public:
-    Target_POI(ros::NodeHandle *nh) {
-        target_idx_ = -1;
-        poi_sub_ = nh->subscribe("/red/poi", 100, &Target_POI::poi_sub_callback, this);
+void Target_POI::poi_sub_callback(const carrot_team::poi &msg) {
+    /* Temporary poi -> permanent poi */
+    int poi_len = msg->poi.size();
+    point_of_interests_.poi.resize(poi_len);
+    passed_poi_.resize(poi_len);
+
+    int _i;
+    for (_i = 0; _i < poi_len; ++i) {
+        point_of_interests_.poi[_i].x = msg->poi[_i].x;
+        point_of_interests_.poi[_i].y = msg->poi[_i].y;
+        point_of_interests_.poi[_i].z = msg->poi[_i].z;
     }
+}
+
+void Target_POI::calculate_range(float *temp_poi, float *current_pos, float *range, float *yaw) {
+    float delta_x, delta_y, delta_z;
+    delta_x = current_pos[0] - temp_poi[0];
+    delta_y = current_pos[1] - temp_poi[1];
+    delta_z = current_pos[2] - temp_poi[2];
+    *range = (delta_x * delta_x) + (delta_y * delta_y) + (delta_z * delta_z);
+    *yaw = atan(delta_x / delta_y);
+}
+
+void Target_POI::find_min_range(float *current_pos) {
+    /* find minimum range among poi */
+    float range;
+    float min_range = 10e9f;
+
+    int len = point_of_interests_->poi.size();
+    float temp_poi[3] = {0, 0, 0};
+    int _i;
+    for (i=0; i<len; ++i) {
+        temp_poi[0] = point_of_interests_->poi[_i].x;
+        temp_poi[1] = point_of_interests_->poi[_i].y;
+        temp_poi[2] = point_of_interests_->poi[_i].z;
+        Target_POI::calculate_range(temp_poi, current_pos, &range, &target_yaw_);
+
+        if (min_range >= range) {
+            min_range = range;
+            target_idx_ = i;
+        }
+    }
+    passed_poi[target_idx_] = true;
+    ROS_INFO("target index of poi is [%d]", target_idx);
+    ROS_INFO("target yaw of poi is [%f]", target_yaw_);
+}
+
+void Target_POI::get_target_poi(float *target_poi_yaw) {
+    /* send the target poi to outside */
+    if (target_idx_ >= 0) {
+        target_poi_yaw[0] = point_of_interests_->poi[target_idx_].x;
+        target_poi_yaw[1] = point_of_interests_->poi[target_idx_].y;
+        target_poi_yaw[2] = point_of_interests_->poi[target_idx_].z;
+        target_poi_yaw[3] = target_yaw_;
+    }
+    else {
+        ROS_INFO("target idx is not defined yet");
+    }
+}
+
+
+
+AIMS::Vehicle::Vehicle(ros::NodeHandle *nh) {
+    current_position_[3] = {0, 0, 0};
     
-    void poi_sub_callback(const carrot_team::poi &msg) {
-        /* Temporary poi -> permanent poi */
-        int poi_len = msg->poi.size();
-        point_of_interests_.poi.resize(poi_len);
-        passed_poi_.resize(poi_len);
+    pos_sub_ = nh->subscribe("/red/pose", 100, &AIMS::Vehicle::pos_sub_callback, this);
 
-        int _i;
-        for (_i = 0; _i < poi_len; ++i) {
-            point_of_interests_.poi[_i].x = msg->poi[_i].x;
-            point_of_interests_.poi[_i].y = msg->poi[_i].y;
-            point_of_interests_.poi[_i].z = msg->poi[_i].z;
-        }
-    }
+    zyaw_pub_ = nh->advertise<geometry_msgs::PoseStamped>("/red/tracker/input_pose", 10);
+}
 
-    void calculate_range(float *temp_poi, float *current_pos, float *range, float *yaw) {
-        float delta_x, delta_y, delta_z;
-        delta_x = current_pos[0] - temp_poi[0];
-        delta_y = current_pos[1] - temp_poi[1];
-        delta_z = current_pos[2] - temp_poi[2];
-        *range = (delta_x * delta_x) + (delta_y * delta_y) + (delta_z * delta_z);
-        *yaw = atan(delta_x / delta_y);
-    }
+void AIMS::Vehicle::pos_sub_callback(const geometry_msgs::PoseStamped &msg) {
+    current_position_[0] = msg->pose.position.x;
+    current_position_[1] = msg->pose.position.y;
+    current_position_[2] = msg->pose.position.z;
+}
 
-    void find_min_range(float *current_pos) {
-        /* find minimum range among poi */
-        float range;
-        float min_range = 10e9f;
- 
-        int len = point_of_interests_->poi.size();
-        float temp_poi[3] = {0, 0, 0};
-        int _i;
-        for (i=0; i<len; ++i) {
-            temp_poi[0] = point_of_interests_->poi[_i].x;
-            temp_poi[1] = point_of_interests_->poi[_i].y;
-            temp_poi[2] = point_of_interests_->poi[_i].z;
-            calculate_range(temp_poi, current_pos, &range, &target_yaw_);
+void AIMS::Vehicle::set_zoffset_yaw(float *target_poi_yaw) {
 
-            if (min_range >= range) {
-                min_range = range;
-                target_idx_ = i;
-            }
-        }
-        passed_poi[target_idx_] = true;
-        ROS_INFO("target index of poi is [%d]", target_idx);
-        ROS_INFO("target yaw of poi is [%f]", target_yaw_);
-    }
+    EulerAngles angle;
+    Quaternion q;
+    angle.yaw = target_poi_yaw[3];
+    q = ToQuaternion(angle);
 
-    void get_target_poi(float *target_poi_yaw) {
-        /* send the target poi to outside */
-        if (target_idx_ >= 0) {
-            target_poi_yaw[0] = point_of_interests_->poi[target_idx_].x;
-            target_poi_yaw[1] = point_of_interests_->poi[target_idx_].y;
-            target_poi_yaw[2] = point_of_interests_->poi[target_idx_].z;
-            target_poi_yaw[3] = target_yaw_;
-        }
-        else {
-            ROS_INFO("target idx is not defined yet");
-        }
-    }
-};
-
-
-namespace AIMS {
-    class Vehicle
-    {
-        private:
-        float current_position_[3];
-        ros::Subscriber pos_sub_;    
-        ros::Publisher zyaw_pub_;
-
-        public:
-        Vehicle(ros::NodeHandle *nh) {
-            current_position_[3] = {0, 0, 0};
-            
-            pos_sub_ = nh->subscribe("/red/pose", 100, &AIMS::Vehicle::pos_sub_callback, this);
-
-            zyaw_pub_ = nh->advertise<geometry_msgs::PoseStamped>("/red/tracker/input_pose", 10);
-        }
-
-        void pos_sub_callback(const geometry_msgs::PoseStamped &msg) {
-            current_position_[0] = msg->pose.position.x;
-            current_position_[1] = msg->pose.position.y;
-            current_position_[2] = msg->pose.position.z;
-        }
-
-        void set_zoffset_yaw(float *target_poi_yaw) {
-
-            EulerAngles angle;
-            Quaternion q;
-            angle.yaw = target_poi_yaw[3];
-            q = ToQuaternion(angle);
-
-            geometry_msgs::PointStamped zyaw_point_msg;
-            zyaw_point_msg.pose.position.z = target_poi_yaw[2];
-            zyaw_point_msg.pose.orientation.w = q.w;
-            zyaw_point_msg.pose.orientation.x = q.x;
-            zyaw_point_msg.pose.orientation.y = q.y;
-            zyaw_point_msg.pose.orientation.z = q.z;
-            zyaw_pub_.publish(zyaw_point_msg;
-            ROS_INFO("Setting zoffset and yaw to target");
-        }
-    };
+    geometry_msgs::PointStamped zyaw_point_msg;
+    zyaw_point_msg.pose.position.z = target_poi_yaw[2];
+    zyaw_point_msg.pose.orientation.w = q.w;
+    zyaw_point_msg.pose.orientation.x = q.x;
+    zyaw_point_msg.pose.orientation.y = q.y;
+    zyaw_point_msg.pose.orientation.z = q.z;
+    zyaw_pub_.publish(zyaw_point_msg;
+    ROS_INFO("Setting zoffset and yaw to target");
 }
 
 
