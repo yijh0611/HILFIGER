04f0ab901ba96b7a49a2c1610b3cf0cbea11db83
yijh0611 <yijh0611@naver.com>
2023-03-12 19:53:12 +0900

    edit error

--- start_hak.sh
+++ start_hak.sh
@@ -10,8 +10,8 @@ cd "$SCRIPTPATH"
 rm .tmuxinator.yml
 
 # link the session file to .tmuxinator.yml
-ln session.yml .tmuxinator.yml
+ln session_hal.yml .tmuxinator.yml
 
 # start tmuxinator
 #tmuxinator
-tmuxinator start -p session.yml
+tmuxinator start -p session_hak.yml
