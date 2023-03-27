51d04aebb3668b4c65f1aa63e2f1760307604a21
yijh0611 <yijh0611@naver.com>
2023-03-27 06:03:40 +0200

    backup

--- src/carrot_team/scripts/mapping_new.py
+++ src/carrot_team/scripts/mapping_new.py
@@ -26,6 +26,14 @@ class Mapping:
         # is get POI end
         self.is_poi = False
 
+        # Constant
+        self.WIDTH = 640
+        self.HEIGHT = 480
+        
+        self.W_HALF = self.WIDTH // 2
+        self.H_HALF = self.HEIGHT // 2
+
+
         # Set variables
         self.x_size = 21 # 15 in introduction
         self.y_size = 51 # 50 in introduction
@@ -53,8 +61,8 @@ class Mapping:
         맵의 크기는 0도 포함해서 16 * 51 * 26으로
         현재는 2D 이기 때문에 16 * 51으로 만들 계획이다.
         '''
-        self.w = 640
-        self.h = 480
+        self.w = self.WIDTH
+        self.h = self.HEIGHT
 
         self.bridge = CvBridge() # Get drone image
         self.img_depth = np.zeros((self.h, self.w))
@@ -74,64 +82,62 @@ class Mapping:
         ########## Radian array cal start ##########
         # get radian - To reduce calculation
         # Constant variable
-        self.w_half = self.w // 2
-        self.h_half = self.h // 2
+        self.W_HALF = self.w // 2
+        self.H_HALF = self.h // 2
 
-        self.w_list = np.array([])
-        self.h_list = np.array([])
-        self.w_tan = np.array([])
-        self.h_tan = np.array([])
+        self.W_LIST = np.array([])
+        self.H_LIST = np.array([])
+        self.W_TAN = np.array([])
+        self.H_TAN = np.array([])
 
-        self.prop_w_s = 60
-        self.prop_w_l = 580
-        self.prop_h_s = 100
-        self.prop_h_l = 180 # 177
+        self.PROP_W_S = 60
+        self.PROP_W_L = 580
+        self.PROP_H_S = 100
+        self.PROP_H_L = 180 # 177
 
         self.res = 10 # open space mapping resolution
 
         # Change if necessary
-        self.w_rad = math.radians(87)
-        self.h_rad = math.radians(58)
+        self.W_RAD = math.radians(87)
+        self.H_RAD = math.radians(58)
 
-        self.w_skip = 5
-        self.h_skip = 5
+        self.W_SKIP = 5
+        self.H_SKIP = 5
 
         # change prop range
-        self.prop_w_s = self.prop_w_s // self.w_skip
-        self.prop_w_l = self.prop_w_l // self.w_skip
-        self.prop_h_s = self.prop_h_s // self.h_skip
-        self.prop_h_l = self.prop_h_l // self.h_skip
+        self.PROP_W_S = self.PROP_W_S // self.W_SKIP
+        self.PROP_W_L = self.PROP_W_L // self.H_SKIP
+        self.PROP_H_S = self.PROP_H_S // self.H_SKIP
+        self.PROP_H_L = self.PROP_H_L // self.H_SKIP
         
 
         for i in range(self.h):
-            h = self.h_half - i
+            h = self.H_HALF - i
             if h <= 0:
                 h -= 1
             
-            rad_h = math.atan((math.tan(self.h_rad / 2) * h) / self.h_half)
+            rad_h = math.atan((math.tan(self.H_RAD / 2) * h) / self.H_HALF)
 
-            self.h_list = np.append(self.h_list, rad_h)
-            self.h_tan = np.append(self.h_tan, math.tan(rad_h))
+            self.H_LIST = np.append(self.H_LIST, rad_h)
+            self.H_TAN = np.append(self.H_TAN, math.tan(rad_h))
         
         for i in range(self.w):
-            w = i - self.w_half
+            w = i - self.W_HALF
             if w >= 0:
                 w += 1
             
-            rad_w = math.atan((math.tan(self.w_rad / 2) * w) / self.w_half)
-
-            self.w_list = np.append(self.w_list, rad_w)
-            self.w_tan = np.append(self.w_tan, math.tan(rad_w))
+            rad_w = math.atan((math.tan(self.W_RAD / 2) * w) / self.H_RAD)
+            self.W_LIST = np.append(self.W_LIST, rad_w)
+            self.W_TAN = np.append(self.W_TAN, math.tan(rad_w))
 
         # Skip array
-        self.h_list = self.h_list[::self.h_skip]
-        self.w_list = self.w_list[::self.w_skip]
-        self.h_tan = self.h_tan[::self.h_skip]
-        self.w_tan = self.w_tan[::self.w_skip]
-
+        self.H_LIST = self.H_LIST[::self.H_SKIP]
+        self.W_LIST = self.W_LIST[::self.W_SKIP]
+        self.H_TAN = self.H_TAN[::self.H_SKIP]
+        self.W_TAN = self.W_TAN[::self.H_SKIP]
         # Change shape to horizontal array
-        self.h_list = np.reshape(self.h_list, (-1, 1))
-        self.h_tan = np.reshape(self.h_tan, (-1, 1))
+        self.H_LIST = np.reshape(self.H_LIST, (-1, 1))
+        self.H_TAN = np.reshape(self.H_TAN, (-1, 1))
 
         ########## Radian array cal end ##########
         
@@ -160,7 +166,6 @@ class Mapping:
         rospy.spin()
 
     def image_callback_depth(self, msg):
-
         # get depth image
         tmp = np.array(self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')) * 1
         
@@ -186,25 +191,6 @@ class Mapping:
 
         self.drone_pose = tmp
 
-    # def get_dist(self, d, w, h, w_all = 640, h_all = 480, rad_cam_w = math.radians(87), rad_cam_h = math.radians(58)): # 앞에거가 원래 58이었음.
-    #     rad_w = self.w_list[w]
-    #     rad_h = self.h_list[h]
-
-    #     dist_x = d * math.tan(rad_w)
-    #     dist_y = d
-    #     dist_z = d * math.tan(rad_h)
-
-    #     return dist_x, dist_y, dist_z
-
-    # def get_r(self, d, n, w = 640, rad_cam = math.radians(58)):
-    #     w_half = w // 2
-    #     rad_cam_half = rad_cam / 2
-
-    #     r_x = (n) / (n ** 2 + (w_half / math.tan(rad_cam_half)) ** 2)**0.5
-    #     r_y = (w_half) / (math.tan(rad_cam_half) * (n ** 2 + (w_half / math.tan(rad_cam_half)) ** 2) ** 0.5)
-
-    #     return r_x, r_y
-
     def is_poi_callback(self, msg):
         self.is_poi = msg.data
     
@@ -212,8 +198,6 @@ class Mapping:
         while True:
             array = self.map_np * 1
             array_flat = np.reshape(self.map_np, -1).astype(np.int32)
-
-            # print(np.shape(array))
 
             msg = Int32MultiArray()
             # msg.data = sum(array, []) # flatten the 3D array to 1D list
@@ -253,158 +237,172 @@ if __name__ == "__main__" :
         time.sleep(1)
         if time.time() - time_is_poi > 40:
             print('POI not ready; Error!')
+            print("Mapping Start")
             break
 
     while True:
-        width = 640
-        height = 480
-        w_half = width // 2
-        h_half = height // 2
-
-        wall_x = np.array([])
-        wall_y = np.array([])
-        wall_z = np.array([])
-
-        open_x = np.array([])
-        open_y = np.array([])
-        open_z = np.array([])
-
-        # Rotation matrix
-        # rot = np.array([[math.cos(mp.drone_yaw), -1 * math.sin(mp.drone_yaw)],[math.sin(mp.drone_yaw), math.cos(mp.drone_yaw)]])
-        cy = math.cos(mp.drone_yaw)
-        sy = math.sin(mp.drone_yaw)
-
-        # change nan to 10
-        img_d_y = np.nan_to_num(mp.img_depth, nan = 10) * mp.mask
-        img_d_y = img_d_y[::mp.h_skip,::mp.w_skip]
-        img_d_x = img_d_y * mp.w_tan
-        img_d_z = img_d_y * mp.h_tan
-
-        img_d_x_new = cy * img_d_x - sy * img_d_y # rotated
-        img_d_y_new = cy * img_d_y + sy * img_d_x # rotated
-
-        # flatten
-        d_flat = np.reshape(img_d_y, (-1))
-        d_y_flat = np.reshape(img_d_y_new, (-1))
-        d_x_flat = np.reshape(img_d_x_new, (-1))
-        d_z_flat = np.reshape(img_d_z, (-1))
-
-        # remove propeller
-        idx = np.array(np.nonzero(d_flat))[0]
-        d_flat = d_flat[idx]
-        d_y_flat = d_y_flat[idx]
-        d_x_flat = d_x_flat[idx]
-        d_z_flat = d_z_flat[idx]
-
-        # Open space
-        for i in range(1, mp.res):
-            open_x = np.append(open_x, d_x_flat * i / mp.res)
-            open_y = np.append(open_y, d_y_flat * i / mp.res)
-            open_z = np.append(open_z, d_z_flat * i / mp.res)
-
-        # wall mapping
-        idx = np.where(d_flat == 10)[0]
-        wall_x = np.delete(d_x_flat, idx)
-        wall_y = np.delete(d_y_flat, idx)
-        wall_z = np.delete(d_z_flat, idx)
-
-
-
-
-
-        # Check position change
-        mp.is_global_mapping = False
-        for i in range(4):
-            if i < 3:
-                if mp.pos_prev[i] != mp.drone_pose[i]:
-                    mp.is_global_mapping = True
-                    break
+        # Map when drone is still
+        if time.time() - mp.time_is_map > 0.2:
+            # Check position change
+            mp.is_global_mapping = False
+            for i in range(4):
+                if i < 3:
+                    if mp.pos_prev[i] != mp.drone_pose[i]:
+                        mp.is_global_mapping = True
+                    mp.pos_prev[i] = mp.drone_pose[i]
+                else: # i == 3
+                    if mp.pos_prev[i] != mp.drone_yaw:
+                        mp.is_global_mapping = True
+                    mp.pos_prev[i] = mp.drone_yaw
             
-            if i == 3:
-                if mp.pos_prev[i] != mp.drone_yaw:
-                    mp.is_global_mapping = True
+            if mp.is_global_mapping:
+                mp.time_total = time.time()
+                wall_x = np.array([])
+                wall_y = np.array([])
+                wall_z = np.array([])
 
-        # global mapping
-        if mp.is_global_mapping:
-            if time.time() - mp.time_is_map > 0.2 and (len(wall_x) > 0 or len(open_x) > 0):
-                # mapping when drone is still for more than 0.5s.
-                for i in range(len(wall_x)):
-                    map_x = int(mp.drone_pose[0] + wall_y[i]) # !! 드론에 더 가까운 쪽으로 벽을 만들 필요가 있기 때문에, 그냥 int를 쓰면 안되고 상황에 따라서 +- 1을 해야한다. - 일단 맵이 어떻게 되는지 확인 후 기능 추가
-                    map_y = int(mp.drone_pose[1] - wall_x[i])
-                    map_z = int(mp.drone_pose[2] + wall_z[i])
+                open_x = np.array([])
+                open_y = np.array([])
+                open_z = np.array([])
+
+                # Rotate
+                cy = math.cos(mp.drone_yaw)
+                sy = math.sin(mp.drone_yaw)
+
+                # change nan to 10
+                img_d_y = np.nan_to_num(mp.img_depth, nan = 10) * mp.mask
+                img_d_y = img_d_y[::mp.H_SKIP,::mp.W_SKIP]
+                img_d_x = img_d_y * mp.H_SKIP
+                img_d_z = img_d_y * mp.H_TAN
+
+                img_d_x_new = cy * img_d_x - sy * img_d_y # rotated
+                img_d_y_new = cy * img_d_y + sy * img_d_x # rotated
+
+                # flatten
+                d_flat = np.reshape(img_d_y, (-1))
+                d_y_flat = np.reshape(img_d_y_new, (-1))
+                d_x_flat = np.reshape(img_d_x_new, (-1))
+                d_z_flat = np.reshape(img_d_z, (-1))
+
+                # remove propeller
+                idx = np.array(np.nonzero(d_flat))[0]
+                d_flat = d_flat[idx]
+                d_y_flat = d_y_flat[idx]
+                d_x_flat = d_x_flat[idx]
+                d_z_flat = d_z_flat[idx]
+
+                # Open space
+                for i in range(1, mp.res):
+                    open_x = np.append(open_x, d_x_flat * i / mp.res)
+                    open_y = np.append(open_y, d_y_flat * i / mp.res)
+                    open_z = np.append(open_z, d_z_flat * i / mp.res)
+
+                # wall mapping
+                idx = np.where(d_flat == 10)[0]
+                wall_x = np.delete(d_x_flat, idx)
+                wall_y = np.delete(d_y_flat, idx)
+                wall_z = np.delete(d_z_flat, idx)
+
+
+                # global mapping
+                if (len(wall_x) > 0 or len(open_x) > 0):
+                    map_x = (mp.drone_pose[0] + wall_y).astype(int)
+                    map_y = (mp.drone_pose[1] - wall_x).astype(int)
+                    map_z = (mp.drone_pose[2] + wall_z).astype(int)
                     
-                    try:
-                        mp.wall_np[map_x, map_y, map_z] += 1
-                        # if mp.map_np[map_x, map_y, map_z] == 0:
-                        if mp.wall_np[map_x, map_y, map_z] * 10 >= mp.open_np[map_x, map_y, map_z]:
-                            mp.map_np[map_x, map_y, map_z] = 2 # Wall
-                            mp.map_img[mp.x_size - map_x, mp.y_size - map_y, map_z, 2] = 125 # 빨간색
-                    except:
-                        pass
-                
-                for i in range(len(open_x)):
-                    map_x = int(mp.drone_pose[0] + open_y[i]) # !! 여기서도 문제가 있을 수도 있으니 결과 보고 수정 필요하면 수정하기.
-                    map_y = int(mp.drone_pose[1] - open_x[i])
-                    map_z = int(mp.drone_pose[2] + open_z[i])
+                    for i in range(len(wall_x)):
+                        mx = map_x[i]
+                        my = map_y[i]
+                        mz = map_z[i]
 
-                    try:
-                        mp.open_np[map_x, map_y, map_z] += 1
-                        # if mp.map_np[map_x, map_y, map_z] == 0:
-                        if mp.open_np[map_x, map_y, map_z] > mp.wall_np[map_x, map_y, map_z] * 10:
-                            mp.map_np[map_x, map_y, map_z] = 1 # Open space
-                            mp.map_img[mp.x_size - map_x, mp.y_size - map_y, map_z, :] = 125
-                            # print(map_x, map_y, map_z)
-                    except:
-                        pass
+                        mp.wall_np[mx, my, mz] += 10
+                        if mp.wall_np[mx, my, mz] >= mp.open_np[mx, my, mz]:
+                            mp.map_np[mx, my, mz] = 2 # Wall
+                            mp.map_img[mx, my, mz, 2] = 125 # 빨간색
 
-        mul = 20
-        img = cv2.resize(mp.map_img[:, :, int(mp.drone_pose[2]), :], dsize = (mp.y_size * mul, mp.x_size * mul))
-        cv2.imshow('Global map', img)
-        key = cv2.waitKey(10)
 
-        if key == ord('q'):
-            break
 
-        # # # 갈 수 있는 곳과 갈 수 없는 곳 둘다 매핑해서 Plot 하는 부분
-        if len(wall_x) > 0:
-            print(len(wall_x))
-            fig = plt.figure()
-            ax = fig.add_subplot(1, 2, 1, projection = '3d')
-            ax.scatter(wall_x, wall_y, wall_z, marker = '.')
-            # plt.grid(True)
-            # ax.title('3D')
 
-            # plt.subplot(2,1,2)
-            # new map temp
-            # h_tmp = int(mp.drone_pose[2])
-            wall_x_tmp = np.array([])
-            wall_y_tmp = np.array([])
-            # print('wall_z :', len(wall_z))
-            for i,n in enumerate(wall_z):
-                if int(n) == 0:
-                    wall_x_tmp = np.append(wall_x_tmp, wall_x[i])
-                    wall_y_tmp = np.append(wall_y_tmp, wall_y[i])
-                
-            ax = fig.add_subplot(1, 2, 2)
-            ax.scatter(open_x, open_y)
-            ax.scatter(wall_x_tmp, wall_y_tmp)
-            # ax.scatter(wall_x, wall_y)
-            ax.grid(True)
-            ax.scatter(0, 0)
-            # ax.title('2D')
-            
-            # # plt.show()
+                        # map_x = int(mp.drone_pose[0] + wall_y[i]) # !! 드론에 더 가까운 쪽으로 벽을 만들 필요가 있기 때문에, 그냥 int를 쓰면 안되고 상황에 따라서 +- 1을 해야한다. - 일단 맵이 어떻게 되는지 확인 후 기능 추가
+                        # map_y = int(mp.drone_pose[1] - wall_x[i])
+                        # map_z = int(mp.drone_pose[2] + wall_z[i])
+                        
+                        # # try:
+                        # mp.wall_np[map_x, map_y, map_z] += 10
+                        # # # if mp.map_np[map_x, map_y, map_z] == 0:
+                        # if mp.wall_np[map_x, map_y, map_z] >= mp.open_np[map_x, map_y, map_z]:
+                        #     mp.map_np[map_x, map_y, map_z] = 2 # Wall
+                        #     mp.map_img[map_x, map_y, map_z, 2] = 125 # 빨간색
+                        # except:
+                        #     count += 1
+                    # print(f"Error mapping wall {count}/{len(wall_x)} times")
+                    
+                    for i in range(len(open_x)):
+                        map_x = int(mp.drone_pose[0] + open_y[i]) # !! 여기서도 문제가 있을 수도 있으니 결과 보고 수정 필요하면 수정하기.
+                        map_y = int(mp.drone_pose[1] - open_x[i])
+                        map_z = int(mp.drone_pose[2] + open_z[i])
 
-            # 이미지 저장 후 다시 불러와서 imshow
-            filename = 'plot.png'
-            fig.savefig(filename)
+                        try:
+                            mp.open_np[map_x, map_y, map_z] += 1
+                            # if mp.map_np[map_x, map_y, map_z] == 0:
+                            if mp.open_np[map_x, map_y, map_z] > mp.wall_np[map_x, map_y, map_z] * 10:
+                                mp.map_np[map_x, map_y, map_z] = 1 # Open space
+                                mp.map_img[map_x, map_y, map_z, :] = 125
+                                # print(map_x, map_y, map_z)
+                        except:
+                            pass
 
-            img = cv2.imread(filename)
+                mul = 20
+                # mp.map_img_tmp = np.flip(mp.map_img, (0,1))
+                mp.map_img_tmp = mp.map_img * 1
+                # print(np.shape(mp.map_img_tmp))
 
-            cv2.imshow("Local mapping", img)
-            key = cv2.waitKey(10)
+                img = cv2.resize(mp.map_img_tmp[:, :, int(mp.drone_pose[2]), :], dsize = (mp.y_size * mul, mp.x_size * mul))
+                cv2.imshow('Global map', img)
+                key = cv2.waitKey(10)
 
-        print(time.time() - mp.time_total)
-        mp.time_total = time.time()
+                if key == ord('q'):
+                    break
+
+                # # # 갈 수 있는 곳과 갈 수 없는 곳 둘다 매핑해서 Plot 하는 부분
+                if len(wall_x) > 0:
+                    print(len(wall_x))
+                    fig = plt.figure()
+                    ax = fig.add_subplot(1, 2, 1, projection = '3d')
+                    ax.scatter(wall_x, wall_y, wall_z, marker = '.')
+                    # plt.grid(True)
+                    # ax.title('3D')
+
+                    # plt.subplot(2,1,2)
+                    # new map temp
+                    # h_tmp = int(mp.drone_pose[2])
+                    wall_x_tmp = np.array([])
+                    wall_y_tmp = np.array([])
+                    # print('wall_z :', len(wall_z))
+                    for i,n in enumerate(wall_z):
+                        if int(n) == 0:
+                            wall_x_tmp = np.append(wall_x_tmp, wall_x[i])
+                            wall_y_tmp = np.append(wall_y_tmp, wall_y[i])
+                        
+                    ax = fig.add_subplot(1, 2, 2)
+                    ax.scatter(open_x, open_y)
+                    ax.scatter(wall_x_tmp, wall_y_tmp)
+                    # ax.scatter(wall_x, wall_y)
+                    ax.grid(True)
+                    ax.scatter(0, 0)
+                    # ax.title('2D')
+                    
+                    # # plt.show()
+
+                    # 이미지 저장 후 다시 불러와서 imshow
+                    filename = 'plot.png'
+                    fig.savefig(filename)
+
+                    img = cv2.imread(filename)
+
+                    # cv2.imshow("Local mapping", img)
+                    # key = cv2.waitKey(10)
+
+                print(time.time() - mp.time_total)
 
     cv2.destroyAllWindows()
